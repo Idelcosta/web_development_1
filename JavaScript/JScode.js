e850463258125c5f30505b1c738ce0e1fe649a57
Idelcosta <mbutuyeidelcosta@gmail.com>
2023-06-13 17:57:22 +0300

    adding games

--- JavaScript/JScode.js
+++ JavaScript/JScode.js
@@ -170,3 +170,12 @@ Dominating the DOM to add functionality to HTML elts
 any html objects has two...: property (.innerhtml, .style, .firstChild) and methods(.click(), .appendChild(), .setAttribute())
 all of them accessed with javaScript by using a .notation
 
+document.querySelector("...");
+document.querySelector("button").classList; gives the lists of classes attached to it
+document.querySelector("button").classList.add("invisible"); adds new class invisible
+document.querySelector("h1").classList.add("huge"); change the h1 with the css huge by using javaScript
+
+The attributes are: class, href... all colored in orange
+document.querySelector("a").attributes;
+document.querySelector("a").getAttribute("href");
+document.querySelector("a").setAttribute("href", "https://www.bing.com");
